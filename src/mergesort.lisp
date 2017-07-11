0ce331376eabced81d6e8bba4cbdd6c72b8ebb23
Alexandre Rademaker <arademaker@gmail.com>
2017-07-11 17:00:26 -0300

    tiny formatting

--- src/mergesort.lisp
+++ src/mergesort.lisp
@@ -1,6 +1,6 @@
 ;;ideal implementation would be using arrays, right?
+
 (defun mergesort(a-list)
-  ""
   (if (> (length a-list) 1)
       (multiple-value-bind (list1 list2) (halve-list a-list)
 	(merge-lists (mergesort list1) (mergesort list2)))
@@ -8,10 +8,10 @@
 
 (defun halve-list(a-list)
   "halve list into two, if number of elements is odd the first list is
-longer."
+   longer."
   (let ((len (length a-list)))
-    (values (subseq a-list 0 (round (/ len 2))) (subseq
-    a-list (round (/ len 2))))))
+    (values (subseq a-list 0 (round (/ len 2)))
+	    (subseq a-list (round (/ len 2))))))
 
 (defun merge-lists(list1 list2)
   "merge lists according to size of first argument."
