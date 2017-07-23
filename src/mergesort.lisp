aa5bdec6f0391e8b10dcea0ce41dce9d25d2959e
odanoburu <bcclaro@gmail.com>
2017-07-23 17:49:38 -0300

    add inversion count

--- src/mergesort.lisp
+++ src/mergesort.lisp
@@ -3,17 +3,18 @@
 
 ;; ideal implementation would be using arrays.
 
-(defun mergesort-skeleton (a-list a-function &optional (return-function #'identity))
-  "this skeleton is used in mergesort or in merging sorted lists."
-  (if (> (length a-list) 1)
-      (multiple-value-bind (list1 list2) (halve-sequence a-list)
-	(funcall a-function
-		 (mergesort-skeleton list1 a-function return-function)
-		 (mergesort-skeleton list2 a-function return-function)))
-      (funcall return-function a-list)))
+(defun mergesort-skeleton
+    (seq merge-fn &optional (return-fn #'identity))
+  "this skeleton is used in mergesort or in merging sorted sequences."
+  (if (> (length seq) 1)
+      (multiple-value-bind (seq1 seq2) (halve-sequence seq)
+	(funcall merge-fn
+		 (mergesort-skeleton seq1 merge-fn return-fn)
+		 (mergesort-skeleton seq2 merge-fn return-fn)))
+      (funcall return-fn seq)))
 
 (defun halve-sequence (seq)
-  "halves a sequence into two, if number of elements is odd the first
+  "halve a sequence into two, if number of elements is odd the first
    list is longer."
   (let ((len (length seq)))
     (values (subseq seq 0 (ceiling (/ len 2)))
@@ -29,15 +30,44 @@
 	  (t (cons car2 (merge-lists list1 (rest list2)))))))
 
 (defun mergesort (a-list)
-  "sorts a list of integers using the mergesort algorithm."
+  "sort a list of integers using the mergesort algorithm."
   (mergesort-skeleton a-list #'merge-lists))
 
 ;; exercise 2.19 Sanjoy Dasgupta, Christos Papadimitriou, Umesh
 ;; Vazirani --- Algorithms (2006)
 
 (defun merge-sorted-lists (lists)
-  "merges several sorted lists of integers using divide-and-conquer."
+  "merge several sorted lists of integers using divide-and-conquer."
   (mergesort-skeleton lists #'merge-lists #'first))
+
+;; counting inverses algorithm
+;; from coursera course
+
+(defun merge-and-count (vector)
+  "sort a vector made up of integers and return number of inversions
+made."
+  (defparameter *inversions* 0)
+  (let ((sorted-vector (mergesort-skeleton vector #'count-and-merge)))
+    (values *inversions* sorted-vector)))
+
+(defun count-and-merge (vector1 vector2)
+  "merge two vectors in sorting order and counts number of inversions
+made."
+  (let ((len1 (length vector1))
+	(len2 (length vector2)))
+    (if (or (= len1 0) (= len2 0))
+	(concatenate 'vector vector1 vector2)
+	(let ((first1 (svref vector1 0))
+	      (first2 (svref vector2 0)))
+	  (cond ((> first1 first2)
+		 (progn (incf *inversions* len1)
+			(concatenate 'vector (list first2)
+			      (count-and-merge vector1
+					       (subseq vector2 1)))))
+		((<= first1 first2)
+		 (concatenate 'vector (list first1)
+			      (count-and-merge (subseq vector1 1)
+					       vector2))))))))
 
 ;; tests
 
@@ -48,3 +78,8 @@
 (merge-sorted-lists '(nil nil nil nil))
 (merge-sorted-lists '((1 1 1) (1 1 1) (1 1 1)))
 (merge-sorted-lists '((-1 1 11)))
+(merge-and-count #(1 1 0 0))
+(merge-and-count #(0 0 0))
+(merge-and-count #())
+(merge-and-count #(0 1 2 3))
+(merge-and-count #(4 3 2 0 1))
