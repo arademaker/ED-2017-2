8251b1b1b1b9dfe9516fbfbd6868f0088d5c699b
odanoburu <bcclaro@gmail.com>
2017-08-05 17:14:41 -0300

    correct heap-length

--- src/heap.lisp
+++ src/heap.lisp
@@ -38,9 +38,8 @@
 			  (right-child heap ix) parent predicate)))))
   heap)
 
-
-;; start
-
+;;;;;;;;;
+;;start;;
 (defun feed-heap (heap init-values)
   "insert values in heap. values must be list."
   (if (endp init-values)
@@ -75,12 +74,15 @@
   (1- (array-dimension (heap-vector heap) 0)))
 
 (defun heap-length (heap)
-  "returns index of first nil element in array."
-  (position nil (heap-vector heap)))
+  "number of non-nil elements of array"
+  (count-if-not #'null (heap-vector heap)))
+
+(defun heap-nil-ix (heap)
+  (position nil (heap-vector heap) :from-end t))
 
 (defun set-heap-end (heap element)
   "sets last node of heap to the given element."
-  (setf (heap-nth heap (heap-length heap)) element)
+  (setf (heap-nth heap (heap-nil-ix heap)) element)
   heap)
 
 (defun left-child-ix (ix)
@@ -108,7 +110,8 @@
   (heap-nth heap (parent-ix ix)))
 
 (defun heap>= (element1 element2 &optional (predicate #'>=))
-  "same as >= but defines what happens when first element is nil."
+  "same as predicate but defines what happens when first element is
+nil."
   (cond ((null element1)
 	 t)
 	((null element2)
@@ -119,8 +122,8 @@
 ;;operations;;
 (defun heap-insert (heap element)
   "inserts element into heap."
-  (let ((heaplen (heap-length heap)))
-    (heapify-up (set-heap-end heap element) heaplen)))
+  (let ((nil-ix (heap-nil-ix heap)))
+    (heapify-up (set-heap-end heap element) nil-ix)))
 
 (defun heap-swap! (heap ix parent-ix)
   "rotates elements of indexes ix and parent-ix in heap."
@@ -184,3 +187,5 @@ its children."
 (validate-heap (make-heap :vector #(1 3 2 6 9 10 5))) ; #(1 3 2 6 9 10 5)
 ;;(validate-heap #(1 3 2 6 9 10 1)) ; assertion error
 (start-heap 7 :init-values '(5 6 3 2 9 10 1)) ; #(1 3 2 6 9 10 5)
+(heap-delete (start-heap 7 :init-values '(5 6 3 2 9 10 1)) 6) ; #(1 3 2 6 9 10 NIL)
+(start-heap 7 :init-values '("amanhã" "zebra" "amanda" "bobo" "boçal" "18" "yupi") :predicate #'string>=) ; h#(18 amanda amanhã yupi boçal bobo zebra)
