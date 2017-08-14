94259c3735508b5d590f1e426c52007f4c639834
odanoburu <bcclaro@gmail.com>
2017-08-14 17:37:45 -0300

    readd struct

--- src/trie.lisp
+++ src/trie.lisp
@@ -1,17 +1,14 @@
 ;; authors: bruno cuconato (@odanoburu)
 ;; placed in the public domain.
 
+;; any node at any point of a trie is itself a trie (as a trie is
+;; characterized by a root node and its children
 (defstruct (trie (:print-function
-		   (lambda (trie stream k)
-		     (identity k)
-		     (format stream "t#(~A)" (trie-root trie)))))
-  (root nil))
-
-(defstruct (node (:print-function
 		   (lambda (node stream k)
 		     (identity k)
-		     (format stream "n\"~A\"" (node-value node)))))
-  (value (error "must specify value for node.")))
+		     (format stream "n\"~A\"" (trie-value node)))))
+  (value)
+  (children))
 
 (defun read-file (filepath)
   (with-open-file (stream filepath)
@@ -19,28 +16,47 @@
        while line
        collect line)))
 
+(defun str-to-char (string)
+  (coerce string 'list))
+
+(defun search-children-aux (tries character)
+  (when (endp tries)
+    (return-from search-children-aux nil))
+  (let ((trie (first tries)))
+    (if (char= character (trie-value trie))
+	trie
+	(search-children-aux (rest tries) character))))
+
+(defun search-children (trie character)
+  "searches children of a trie node for a value of character"
+  (let ((trie-children (trie-children trie)))
+    (search-children-aux trie-children character)))
+
+(defun search-trie-aux (trie value &optional (chars 0))
+  (let ((result (search-children trie (first value))))
+    (if (or (endp value) (null result))
+	(values result chars)
+	(search-trie-aux result (rest value) (1+ chars)))))
+
 (defun search-trie (trie value)
-  (rest (assoc value trie :test #'char=)))
+  (search-trie-aux trie (str-to-char value)))
 
-(defun insert-node-aux (trie node-value &optional result)
-  (if (= (length node-value) 0)
-      (acons (first result) (rest result) trie)
-      (insert-node-aux trie (subseq node-value 1)
-		       (cons (char node-value 0) result))))
+(defun add-char-to-children (trie char)
+  (push (make-trie :value char) (trie-children trie)))
 
-(defun insert-node (trie node-value)
-  (insert-node-aux trie (reverse node-value)))
+(defun insert-node (trie value)
+  (if (endp value)
+      trie
+      (insert-node (add-char-to-children trie (first value))
+		   (rest value))))
 
-(defun add-node (trie node-value)
-  (let* ((curr (char node-value 0))
-	(searched-trie (search-trie trie curr)))
-    (if (null searched-trie)
-	(insert-node trie node-value)
-	(acons curr (cons searched-trie
-			  (add-node searched-trie
-				    (subseq node-value 1)))
-	       trie))))
-  
+(defun add-node (trie value)
+  (let ((value (str-to-char value)))
+    (multiple-value-bind (result-trie ix)
+	(search-trie-aux trie value)
+    (if (= (length value) ix)
+	trie
+	(insert-node result-trie (subseq value ix))))))
 
 (defun construct-trie (trie node-values)
   (if (endp node-values)
