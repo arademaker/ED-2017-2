905f89b403213fbc2a47f9086953802ca1a5c54b
odanoburu <bcclaro@gmail.com>
2017-08-24 11:07:31 -0300

    start refactoring

--- src/rec-entitities.lisp
+++ src/rec-entitities.lisp
@@ -1,8 +1,9 @@
 ;; authors: bruno cuconato (@odanoburu)
 ;; placed in the public domain.
 
-;; como lidar com pontuação? (mexer no input de entidades?) ou assim
-;; tá ok?
+;; resolver pontuação.
+;; mudar trie para colocar em is-leaf? o id da entitidade.
+;; transformar em função geradora
 
 (ql:quickload :cl-conllu)
 (compile-file #p"~/git/ed-2017-2/src/trie.lisp")
@@ -22,41 +23,18 @@
   (funcall entities :get))
 |#
 
-(let (entities)
-  (defun insert-entity (entity)
-    (push entity entities))
-  (defun show-entities ()
-    entities)
-  (defun reset-entities ()
-    (setf entities nil)))
-
 ;; process entities: join names, add them to entities-found, do things
 ;; to them
 
-(defun join-words (words &optional joined-words)
-;; equiv to (format nil "~{~A~^ ~}" words), but faster
-  (if (endp words)
-      (subseq joined-words 1) ;to remove first space
-      (join-words (rest words) (concatenate 'string (list #\space)
-					      (first words)
-					      joined-words))))
+(defun unwind-entity (entity start end &optional entities)
+  (unless entity
+    (return-from unwind-entity entities))
+  (let ((ent-id (trie-is-leaf? (first entity))))
+    (if ent-id
+        (unwind-entity (rest entity) start (1- end)
+                       (acons ent-id (list start end) entities))
+        (unwind-entity (rest entity) start (1- end)))))
 
-(defun get-token-form (token)
-  (etypecase token
-    (cl-conllu:token (cl-conllu:token-form token))
-    (cl-conllu:mtoken (cl-conllu:mtoken-form token))))
-
-(defun process-entity (entity)
-  (insert-entity (join-words (mapcar (lambda (node-token)
-                                       (get-token-form
-                                        (rest node-token)))
-                                     entity))))
-
-(defun unwind-entity (entity)
-  (when entity
-    (if (trie-is-leaf? (caar entity))
-	(process-entity entity))
-	(unwind-entity (rest entity))))
 
 ;; recognize entities
 
@@ -64,8 +42,7 @@
   "checks if token is in trie, if it is, sees if it is followed by a
 space and returns the space node, else returns the node. if it's not
 in trie returns nil"
-  (let ((trie-node (partially-in-trie?node trie
-					    (get-token-form token))))
+  (let ((trie-node (partially-in-trie?node trie token)))
     (when trie-node
 	(let* ((children (trie-children trie-node))
 	       (space (find #\space children :key #'trie-value)))
@@ -74,86 +51,61 @@ in trie returns nil"
 	      (when (trie-is-leaf? trie-node)
 		(values trie-node token)))))))
 
-(defun aux-recognize-ents-in-tokens (trie token-list &optional entity)
+(defun aux-recognize-ents-in-sentence (trie token-list
+                                       &key entity (iter 0))
   (when (endp token-list)
-    (return-from aux-recognize-ents-in-tokens (values nil entity)))
-  (multiple-value-bind (trie-node token)
-      (token-in-trie? trie (first token-list))
+    (return-from aux-recognize-ents-in-sentence
+      (values nil entity iter)))
+  (let ((trie-node (token-in-trie? trie (first token-list))))
     (cond ((and (null entity)
 		(null trie-node))
-	   (values (rest token-list) entity))
+	   (values (rest token-list) nil iter))
 	  ((and (null trie-node) entity)
-	   (values token-list entity))
+	   (values token-list entity iter))
 ;; not (rest token-list) to give tk a fresh chance, but will only give
 ;; this chance to the last tk. (think of searching for joão almeida de
 ;; castro when only joão almeida and joão almeida de silva are ents:
 ;; only castro will be reconsidered, not 'de'). is that a problem?
 	  (trie-node
-	   (aux-recognize-ents-in-tokens trie-node
+	   (aux-recognize-ents-in-sentence trie-node
 					 (rest token-list)
-					 (acons trie-node token
-						entity))))))
+					 :entity (cons trie-node
+                                                       entity)
+                                         :iter (1+ iter))))))
 
-(defun recognize-ents-in-tokens (trie token-list)
-  (when token-list
-    (multiple-value-bind (rest-token-list entity)
-	(aux-recognize-ents-in-tokens trie token-list)
-      (unwind-entity entity)
-      (recognize-ents-in-tokens trie rest-token-list))))
+(defun recognize-ents-in-sentence (trie token-list
+                                   &key entities (start 0))
+  "return entities found (list (cons ent-id (start end)))"
+  (if (endp token-list)
+      entities
+      (multiple-value-bind (rest-token-list entity iter)
+          (aux-recognize-ents-in-sentence trie token-list)
+        (recognize-ents-in-sentence trie
+                                    rest-token-list
+                                    :entities
+                                    (append (unwind-entity
+                                             entity start iter)
+                                            entities)
+                                    :start (+ start iter)))))
 
-;; prepare input: get tokens and mtokens from sentence
 
-(defun aux-construct-token-list (tokens start end
-				 &key token-list mtoken)
-  (when (endp tokens) (return-from aux-construct-token-list
-			(values nil token-list)))
-;; because an mtoken can be the last one in a sentence.
-  (let* ((token (first tokens))
-	(token-id (cl-conllu:token-id token))
-	(rest-tokens (rest tokens)))
-    (cond ((< token-id start)
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list (cons token
-						       token-list)
-				     :mtoken mtoken))
-	  ((= token-id start)
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list (cons mtoken
-						       token-list)))
-	  ((and (> token-id start) (<= token-id end))
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list token-list))
-	  ((> token-id end)
-	   (values tokens token-list)))))
+;; entry point
 
-(defun construct-token-list (tokens mtokens &optional token-list)
-  "construct list of tokens, replacing tokens by their respective
-mtokens: (pt em o governo) -> (pt no governo)"
-  (if (endp mtokens)
-      (append (reverse token-list) tokens)
-      (let* ((mtoken (first mtokens))
-	     (start (cl-conllu:mtoken-start mtoken))
-	     (end (cl-conllu:mtoken-end mtoken)))
-	(multiple-value-bind (rest-tokens result-token-list)
-	    (aux-construct-token-list tokens start end :mtoken mtoken)
-	  (construct-token-list rest-tokens (rest mtokens)
-				(append result-token-list
-					token-list))))))
-
-(defun cons-tokens-from-sentence (sentence)
-  (construct-token-list (cl-conllu:sentence-tokens sentence)
-			(cl-conllu:sentence-mtokens sentence)))
-
-;; entry points
-
-(defun recognize-ents-in-sentence (trie sentence)
-  (recognize-ents-in-tokens trie
-                            (cons-tokens-from-sentence sentence)))
-
-(defun recognize-ents-in-sentences (trie sentences)
-  (when sentences
-    (recognize-ents-in-sentence trie (first sentences))
-    (recognize-ents-in-sentences trie (rest sentences))))
+(defun recognize-ents-in-sentences (trie sentences
+                                    &key entities (sentid 0))
+  "take trie and list of sentences (list of tokens), return sentid
+with entities recognized in index ix (entity . ix)"
+  (if (endp sentences)
+      entities
+      (let ((entities-in-sent (recognize-ents-in-sentence
+                               trie (first sentences))))
+        (recognize-ents-in-sentences trie
+                                     (rest sentences)
+                                     :entities (acons
+                                                sentid
+                                                entitites-in-sent
+                                                entities)
+                                     :sentid (1+ sentid)))))
 
 ;; count entities (remove is for better performance)
 
