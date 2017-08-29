9054f7d142005c91151589a24b97d0a642129797
Alexandre Rademaker <arademaker@gmail.com>
2017-08-29 15:19:51 -0300

    organizando arquivos

--- src/counting-sort.lisp
+++ src/counting-sort.lisp
@@ -11,7 +11,7 @@
 
 (defun counting-sort(vector min max)
   "takes a vector of integers, its size min and max values, and
-returns it sorted using the counting sort algorithm."
+   returns it sorted using the counting sort algorithm."
   (let* ((empty-count-vector (make-array (- max min -1) :initial-element 0))
 	 (count-vector (count-ints vector empty-count-vector min))
 	 (accu-vector (accumulate-ints count-vector))
@@ -19,20 +19,22 @@ returns it sorted using the counting sort algorithm."
 	 (empty-sorted-vector (make-array size :initial-element 0)))
     (sort-count accu-vector vector empty-sorted-vector min (1- size))))
 
+
 (defun count-ints(vector count-vector min &optional (ix 0))
   "vector is a vector of integers, count-vector is (initially) a zero
-vector, min is the smallerinteger and ix is the current number being
-counted. return count-vector where count-vector[ix] is number of
-elements equal to min+ix in vector."
+   vector, min is the smallerinteger and ix is the current number
+   being counted. return count-vector where count-vector[ix] is number
+   of elements equal to min+ix in vector."
   (if (= ix (length vector))
       count-vector
       (progn (incf (svref count-vector (- (svref vector ix) min)))
 	     (count-ints vector count-vector min (1+ ix)))))
 
+
 (defun accumulate-ints(count-vector &optional (ix 0))
   "turns a count-vector into an accumulated-count-vector, where
-accumulated-count-vector[ix] is the number of elements less than or
-equal to min+ix in original vector."
+   accumulated-count-vector[ix] is the number of elements less than or
+   equal to min+ix in original vector."
   (cond ((= ix (length count-vector))
 	 count-vector)
 	((> ix 0)
@@ -41,15 +43,17 @@ equal to min+ix in original vector."
 	((= ix 0)
 	 (accumulate-ints count-vector (1+ ix)))))
 
+
 (defun get-count(count-vector integer min)
   "gets count or accumulated count of integer from count-vector."
   (svref count-vector (- integer min)))
 
+
 (defun sort-count(count-vector vector sorted-vector min ix)
   "for each element of vector, checks at count-vector how many
-elements are less or equal than it, and places it at the appropriate
-index at sorted-vector. decrements this count in count-vector, and
-goes to the next element."
+   elements are less or equal than it, and places it at the
+   appropriate index at sorted-vector. decrements this count in
+   count-vector, and goes to the next element."
   (if (< ix 0)
       sorted-vector
       (let ((current (svref vector ix)))
